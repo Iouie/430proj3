8a5c7ddffc726955ad68f3cd7c281e2cf2a3c0f6
Stanley Louie <sxl7953@rit.edu>
2019-11-15 21:44:42 -0500

    getting change password

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -50,7 +50,7 @@ const login = (request, response) => {
     req.session.account = Account.AccountModel.toAPI(account);
 
     return res.json({
-      redirect: '/homePage',
+      redirect: '/userPage',
     });
   });
 };
@@ -90,7 +90,7 @@ const signup = (request, response) => {
     savePromise.then(() => {
       req.session.account = Account.AccountModel.toAPI(newAccount);
       return res.json({
-        redirect: '/homePage',
+        redirect: '/userPage',
       });
     });
 
--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ const router = (app) => {
   app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
-  // app.get('/userPage', mid.requiresLogin, controllers.Account.userPage);
+  app.get('/userPage', mid.requiresLogin, controllers.Account.userPage);
   app.get('/', controllers.Account.homePage);
   app.get('/home', controllers.Account.homePage);
 };
