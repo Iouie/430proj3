8b7fa4f83b44c7224a819634984d880e8aa7ae19
Stanley Louie <sxl7953@rit.edu>
2019-12-02 12:46:18 -0500

    ..

--- client/app/maker.js
+++ client/app/maker.js
@@ -38,7 +38,7 @@ const handleCalories = (e) => {
 
 const NavBar = (props) => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-">
+    <div class="navbar navbar-expand-lg navbar-light bg-">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -67,7 +67,7 @@ const NavBar = (props) => {
       </ul>
       <div class="navlink"><a href="/logout">Log out</a></div>
     </div>
-  </nav>
+  </div>
   );
 }
 
